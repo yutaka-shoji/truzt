3294e438b42436b1458a41f40d80fefc800f4595
Shoji, Yutaka <ytk.shoji@gmail.com>
2025-02-25 00:38:59 +0900

    feat: update .gitignore and enhance test fixtures for Excel to JSON conversion

--- tests/conftest.py
+++ tests/conftest.py
@@ -16,3 +16,13 @@ def v3_test_case_wb() -> Workbook:
     with open("sample/sample_v3.xlsx") as file:
         data = json.load(file)
     return data
+
+
+@pytest.fixture
+def v3_test_case_excel_path() -> str:
+    return "sample/sample_v3.xlsx"
+
+
+@pytest.fixture
+def v3_test_case_json_path() -> str:
+    return "sample/sample_v3.json"
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -3,8 +3,7 @@ from pathlib import Path
 import truzt
 
 
-def test_can_convert_wb_to_json():
-    wb_path = "sample/sample_v3.xlsx"
-    json_path = "out/sample_v3.json"
-
-    truzt.utils.convert_wb_to_json(Path(wb_path), Path(json_path))
+def test_can_convert_wb_to_json(v3_test_case_excel_path, tmp_path: Path):
+    # file name without extensionを取得して、jsonファイル名を作成
+    json_path = v3_test_case_excel_path.replace(".xlsx", ".out.json")
+    truzt.utils.convert_wb_to_json(Path(v3_test_case_excel_path), Path(json_path))
